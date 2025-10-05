11b35c2c0d35e804dfab792f8f66c554ae85995b
Deny Esquerre <lular@ucvvirtual.edu.pe>
2025-10-05 16:55:55 -0500

    feat: generate sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.anngoulden.org',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.8,
+};
